dd1695436e3d122aa252078e217369649bba1d2a
kiwitrr2944 <wiktorr2112@gmail.com>
2024-02-20 19:20:43 +0100

    speed limit exceeded dataframe

--- analizedata.py
+++ analizedata.py
@@ -1,3 +1,5 @@
+from turtle import speed
+from numpy import NAN, NaN
 import pandas as pd
 import os
 import glob
@@ -5,44 +7,61 @@ from datetime import datetime
 from getdata import MAXTIMEDIFF 
 from ztmclasses import Position
 
+REASONABLE_SPEED_INFINITY = 100
+SPEED_LIMIT = 50
+
+
 def calc_speed(row, prev) -> float:
-    if prev[3] != row[3]:
+    if prev['VehicleNumber'] != row['VehicleNumber']:
         return 0.0
     
-    x_pos = Position(row[2], row[5])
-    y_pos = Position(prev[2], prev[5])
+    x_pos = Position(row['Lon'], row['Lat'])
+    y_pos = Position(prev['Lon'], prev['Lat'])
     
-    x_time = pd.to_datetime(prev[4])
-    y_time = pd.to_datetime(row[4])
+    x_time = pd.to_datetime(prev['Time'])
+    y_time = pd.to_datetime(row['Time'])
     
     dist = x_pos.distance(y_pos)
     time = (y_time - x_time).total_seconds()
 
-    return dist / time * 3.6
+    speed = dist / time * 3.6
 
-
-def speed_limit(dirpath="."):
-    lines = glob.glob(f"{dirpath}/DATA/LIVE/LINES/*.csv")
-    exceeded = 0
-    all = 0
-    maxspeed = 0
-    for line in lines:
-        df = pd.read_csv(line).drop_duplicates(subset=['VehicleNumber', 'Time'])
-        
-        for row in df.itertuples():
-            if all > 0:                
-                speed = calc_speed(row, prev)
-                
-                if speed >= 50:
-                    exceeded += 1
-
-                if speed >= maxspeed:
-                    print(line, speed)
-                    maxspeed = speed
-
-            all += 1
-            prev = row  
-
+    if speed <= REASONABLE_SPEED_INFINITY:
+        return speed 
     
-    print(exceeded/all)
-    return (exceeded, all)
+    return NaN
+
+
+def sle_line(line, path='.', save=False):
+    path = f"{path}/DATA/LIVE/LINES/{line}.csv"
+    
+    df = pd.read_csv(path).drop_duplicates(subset=['VehicleNumber', 'Time'])
+    
+    all = 0
+    speeds = [NaN]
+    
+    for row in df.iterrows():
+        if all > 0:                
+            speeds.append(calc_speed(row[1], prev[1]))
+        all += 1
+        prev = row
+        
+    df['Speed'] = speeds
+    df = df.loc[df['Speed'] >= 50.0]
+    if save:
+        path = path.removesuffix(f"{line}.csv")
+        os.makedirs(f"{path}/SLE/", exist_ok=True)
+        path = path + f"/SLE/{line}.csv"
+        df.to_csv(path, index=False)
+    
+    return df
+    
+
+def sle(path="."):
+    lines = glob.glob(f"{path}/DATA/LIVE/LINES/*.csv")
+    ret_list = []
+    
+    for line in lines:
+        ret_list.append(sle_line(line.removeprefix(f"{path}/DATA/LIVE/LINES/", path)))
+        
+    return pd.concat(ret_list, ignore_index=True)
